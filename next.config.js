05f823fdba3a02689915785b6b4cd86a4fe75397
tathanh <tathanh200320@gmail.com>
2024-06-22 09:40:18 +0700

    main: fixing page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/special-gift/" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Special_Gift/" : "",
   images: {
     unoptimized: true,
   },
