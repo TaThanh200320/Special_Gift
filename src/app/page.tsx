d74fbbc5442c65b35548af0ea23fbf2cd8f56e09
tathanh <tathanh200320@gmail.com>
2024-06-22 10:49:25 +0700

    main: test gif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import { BoxComponent } from "../components/BoxComponent";
 import Image from "next/image";
-import { LandingPage } from "../components/LandingPage";
 
 export default function Home() {
   return (
-    <BoxComponent>
-      <LandingPage></LandingPage>
-    </BoxComponent>
+    <div className="bg-[#F6F7F2] overflow-x-hidden overflow-y-hidden">
+      <section className="w-full mx-auto min-h-[100vh] xl:max-w-[1230px] xxl:max-w-[1320px] px-[16px] md:px-[30px] xl:px-0">
+        <Image
+          src={
+            "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDB6NDQwZm1pbXprdnU3bmw4M3RpYXZyaHY3Nm93dDljd2tyM2xqOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2dQ3FMaMFccpi/giphy.webp"
+          }
+          width={800}
+          height={800}
+          alt="Image"
+          className="w-screen h-screen object-cover"
+        ></Image>
+      </section>
+    </div>
   );
 }
